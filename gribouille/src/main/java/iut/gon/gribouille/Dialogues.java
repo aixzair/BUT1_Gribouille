505e2ecf465014988d37a7657ac5cf560359b624
alexandre <lerosie221.unicaen.fr>
2023-04-06 15:35:51 +0200

    ajout branche gribouille_tp2

---
+++ gribouille/src/main/java/iut/gon/gribouille/Dialogues.java
@@ -0,0 +1,15 @@
+package iut.gon.gribouille;
+
+public class Dialogues {
+
+	public Dialogues() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean confirmation() {
+		//Alert
+		
+		return true;
+	}
+
+}
