471fa23e7146c0795d9c8a1bcba784ee8e518af8
alexandre <lerosie221.unicaen.fr>
2023-04-06 14:54:34 +0200

    1.4.2

--- tp2/src/main/java/iut/gon/tp2/TP2App.java
+++ tp2/src/main/java/iut/gon/tp2/TP2App.java
@@ -3,11 +3,13 @@ package iut.gon.tp2;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -51,7 +53,8 @@ public class TP2App extends Application {
   private void prepareBoutons() {
     ajouteTout.setOnAction(this::onAjouteTout);
     retireTout.setOnAction(this::onRetireTout);
-    // TODO actions des deux boutons centraux
+    this.versDroite.setOnAction(this::onVersDroite);
+    this.versGauche.setOnAction(this::onVersGauche);
   }
 
   /** Ajoute tous les éléments de gauche dans la liste de droite
@@ -65,7 +68,28 @@ public class TP2App extends Application {
   /** Ajoute tous les éléments de droite dans la liste de gauche
    Active le bouton "Ajouter tout" et désactive le bouton "Retirer tout" */
   private void onRetireTout(ActionEvent actionEvent) {
-    //TODO
+	  gauche.getItems().addAll(droite.getItems());
+	  droite.getItems().clear();
+  }
+  
+  private void onVersDroite(ActionEvent actionEvent) {
+	  int index = this.gauche.getSelectionModel().getSelectedIndex();
+	  
+	  if (index != -1) {
+		  this.droite.getItems().add(this.gauche.getItems().get(index));
+		  this.gauche.getItems().remove(index);
+		  this.gauche.getSelectionModel().clearSelection();
+	  }
+  }
+  
+  private void onVersGauche(ActionEvent actionEvent) {
+	  int index = this.droite.getSelectionModel().getSelectedIndex();
+	  
+	  if (index != -1) {
+		  this.gauche.getItems().add(this.droite.getItems().get(index));
+		  this.droite.getItems().remove(index);
+		  this.droite.getSelectionModel().clearSelection();
+	  }
   }
 
   /** Prépare les menus et leurs événements */
@@ -80,6 +104,27 @@ public class TP2App extends Application {
     
     fichiers.getItems().add(quitter);
     aide.getItems().add(propos);
+    
+    quitter.addEventHandler(
+    		ActionEvent.ACTION, 
+    		(e) -> { 
+    			Platform.exit();
+    		}
+    );
+    
+    propos.addEventHandler(
+    		ActionEvent.ACTION,
+    		(e) -> { 
+    			Alert alert = new Alert(
+    					AlertType.NONE,
+    					"Faits par Alexandre Lerosier.",
+    					ButtonType.CLOSE
+    			);
+    			
+    			alert.setTitle("À propos");
+    			alert.show();
+    		}
+    );
   }
 
   /**
@@ -87,7 +132,9 @@ public class TP2App extends Application {
    Active le bouton "Ajouter tout"
    */
   private void prepareListe() {
-    //TODO active le bouton "Ajouter tout"
+    this.gauche.getItems().addAll("Element 1", "Element 2");
+    this.ajouteTout.setDisable(false);
+    this.retireTout.setDisable(false);
   }
 
   private void extraitIds(Scene scene) {
