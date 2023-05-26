a1347dd29b6e4580fe12e6219648ff51eb7a1cd6
alex <lerosie221@unicaen.fr>
2023-05-26 12:33:48 +0200

    1.4 fini

--- tp5/src/main/java/fr/unicaen/iut/tp5/ControleurDemineur.java
+++ tp5/src/main/java/fr/unicaen/iut/tp5/ControleurDemineur.java
@@ -3,10 +3,15 @@ package fr.unicaen.iut.tp5;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 public class ControleurDemineur {
 	private ModeleDemineur modele;
@@ -20,17 +25,38 @@ public class ControleurDemineur {
 		this.modele = _modele;
 	}
 	
-	public void initialize(URL location, ResourceBundle resources) {
+	public void initialize() {
+		this.initGrille("10;10;10");
+		
 		this.inconues.textProperty().bind(this.modele.nbInconnuesProperty().asString());
 		this.marques.textProperty().bind(this.modele.nbMarquesProperty().asString());
 		
-		difficulte.selectedToggleProperty().addListener(observable -> {
-		   
+		difficulte.selectedToggleProperty().addListener(new ChangeListener<>() {
+			
+			@Override
+			public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+				initGrille((String) newValue.getUserData());
+				System.out.println("changed");
+			}
 		});
 	}
 	
 	private void initGrille(String userData) {
+		int userDatas[];
 		
+		grille.getColumnConstraints().clear();
+        grille.getRowConstraints().clear();
+        grille.setGridLinesVisible(true); 
+        
+        userDatas = ModeleDemineur.parseUserData(userData);
+        this.modele.setTaille(userDatas[0], userDatas[1], userDatas[2]);
+        
+        for (byte i = 0; i < userDatas[0]; i++) {
+        	grille.getColumnConstraints().add(new ColumnConstraints(32));
+        }
+        
+        for (byte i = 0; i < userDatas[1]; i++) {
+        	grille.getRowConstraints().add(new RowConstraints(32));
+        }
 	}
-
 }
--- tp5/src/main/java/module-info.java
+++ tp5/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.tp5 {
   requires javafx.controls;
   requires javafx.fxml;
   requires javafx.graphics;
+requires javafx.base;
 
   exports fr.unicaen.iut.tp5;
   opens fr.unicaen.iut.tp5 to javafx.fxml;
