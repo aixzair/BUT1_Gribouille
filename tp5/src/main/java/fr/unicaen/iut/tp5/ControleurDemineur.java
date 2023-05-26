59056fe9b13187367f48c0f8e260590a0e1d3fca
alex <lerosie221@unicaen.fr>
2023-05-26 11:13:20 +0200

    1.3 fini

--- tp5/src/main/java/fr/unicaen/iut/tp5/ControleurDemineur.java
+++ tp5/src/main/java/fr/unicaen/iut/tp5/ControleurDemineur.java
@@ -1,20 +1,36 @@
 package fr.unicaen.iut.tp5;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
+
 public class ControleurDemineur {
 	private ModeleDemineur modele;
 	
+	private @FXML ToggleGroup difficulte;
 	private @FXML TextField inconues;
 	private @FXML TextField marques;
-	private @FXML GridePane grille;
-	
-	objet."propriete"Property().bind(valeurAObserver);
-	
+	private @FXML GridPane grille;
+		
 	public ControleurDemineur(ModeleDemineur _modele) {
 		this.modele = _modele;
 	}
 	
 	public void initialize(URL location, ResourceBundle resources) {
-		this.inconues.TextProperty().bind(this.modele.getnbMarques);
+		this.inconues.textProperty().bind(this.modele.nbInconnuesProperty().asString());
+		this.marques.textProperty().bind(this.modele.nbMarquesProperty().asString());
+		
+		difficulte.selectedToggleProperty().addListener(observable -> {
+		   
+		});
+	}
+	
+	private void initGrille(String userData) {
+		
 	}
 
 }
--- tp5/src/main/java/fr/unicaen/iut/tp5/MainDemineur.java
+++ tp5/src/main/java/fr/unicaen/iut/tp5/MainDemineur.java
@@ -1,28 +1,35 @@
 package fr.unicaen.iut.tp5;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class MainDemineur extends Application{
-	ModeleDemineur modele = new ModeleDemineur();
-	ControleurDemineur controleur = new ControleurDemineur();
+	private static Scene scene;
 	
 	@Override
 	public void start(Stage stage) throws Exception {
-		stage.setScene(new Scene(new Label("Ok !"), 800, 600));
+		ModeleDemineur modele = new ModeleDemineur(10, 10, 10);
+		ControleurDemineur controleur = new ControleurDemineur(modele);
+		
+		MainDemineur.scene = new Scene(loadFXML("demineur", controleur), 640, 480);
+		stage.setTitle("démineur");
+		stage.setScene(MainDemineur.scene);
 		stage.show();
 	}
 	
-	private static Parent loadFXML(String fichier, Controleur controleur)
+	private static Parent loadFXML(String fichier, ControleurDemineur controleur)
 	throws IOException {
-	    FXMLLoader fxmlLoader = new FXMLLoader(monApplication.class.getResource(fichier + ".fxml"));
+	    FXMLLoader fxmlLoader = new FXMLLoader(MainDemineur.class.getResource(fichier + ".fxml"));
 	    fxmlLoader.setController(controleur);
 	    return fxmlLoader.load();
 	}
 	
 	public static void main(String[] args) {
-		launch(args);
+		launch();
 	}
 }
